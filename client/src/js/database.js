89c45f7a6bc14f9eb6de4bbff87e8f022fe52de4
David <grayd500@gmail.com>
2023-12-11 12:05:17 -0500

    Starter Code and initial updates for server functionality

---
+++ client/src/js/database.js
@@ -0,0 +1,61 @@
+// client/src/js/database.js:
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// ✅: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+
+  //🆕 Create a connection to the database and specify the version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  //🆕 Create a new transaction and specify the store and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //🆕 Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  //🆕 Use the .add() method on the store and pass in the content.
+  const request = store.add({ jate: content });
+
+  //🆕 Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+
+// ✅: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET all from the database');
+
+  //🆕 Create a connection to the database and specify the version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  //🆕 Create a new transaction and specify the store and the privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  //🆕 Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  //🆕 Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  //🆕 Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,69 @@
+// client/webpack.config.js:
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// ✅: Add and configure workbox plugins for a service worker and manifest file.
+// ✅: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      //🆕
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Edit your text or code on the go!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
