b2a1a2bd8e056e08c5c64e02dcb116a3da4675d5
David <grayd500@gmail.com>
2023-12-12 15:44:57 -0500

    update database.js to fix issues

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,55 +1,55 @@
 // client/src/js/database.js:
+// Import the openDB function from the 'idb' library for IndexedDB operations
 import { openDB } from 'idb';
 
-// Initialize the database
+// Initialize the database, creating it if it doesn't exist, using version 1
 const initdb = async () => {
-  try {
-    await openDB('jate', 1, {
-      upgrade(db) {
-        if (!db.objectStoreNames.contains('jate')) {
-          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-          console.log('jate database created');
-        } else {
-          console.log('jate database already exists');
-        }
-      },
-    });
-  } catch (error) {
-    console.error('Error initializing the database', error);
-  }
+  // Creating a new database or upgrading an existing one to version 1
+  openDB('jate', 1, {
+    // Add database schema if it hasn't already been initialized
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      // Create a new object store for data with a key named 'id' that auto-increments
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
 };
 
-// Add logic to a method that accepts some content and adds it to the database
-// Get the last record from the database
+// Add logic to a method that accepts content and adds it to the database
+export const putDb = async (content) => {
+  // Connect to the 'jate' database with version 1
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction for read and write privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Open the object store
+  const store = tx.objectStore('jate');
+  // Put the content into the object store with a specified ID
+  const request = store.put({ id: 1, value: content });
+  // Confirm that the data was added successfully
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+// Add logic for a method that retrieves all content from the database
 export const getDb = async () => {
-  try {
-    const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const getAllKeysRequest = store.getAllKeys();
-    const keys = await getAllKeysRequest;
-    const lastKey = keys.at(-1); // Get the last key (most recent entry)
-    const result = await store.get(lastKey);
-    console.log('Data fetched from the database', result);
-    return result?.jate;
-  } catch (error) {
-    console.error('Error getting data from the database', error);
-  }
+  console.log('GET from the database');
+  // Connect to the 'jate' database with version 1
+  const jateDB = await openDB('jate', 1);
+  // Create a new transaction for read-only privileges
+  const tx = jateDB.transaction('jate', 'readonly');
+  // Open the desired object store
+  const store = tx.objectStore('jate');
+  // Get all data from the object store
+  const request = store.getAll();
+  // Get confirmation of the request
+  const result = await request;
+  console.log(result);
+  return result;
 };
 
-
-// Add logic for a method that gets the content from the database
-export const getDb = async () => {
-  try {
-    const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const result = await store.get(1);
-    console.log('Data fetched from the database', result);
-    return result?.jate;
-  } catch (error) {
-    console.error('Error getting data from the database', error);
-  }
-};
-
+// Initialize the database when the script is run
 initdb();
