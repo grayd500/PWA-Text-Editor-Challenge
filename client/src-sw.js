33166855d53b5efa8a2c9d18cb54da7f89ec5859
David <grayd500@gmail.com>
2023-12-12 16:01:54 -0500

    fix issue with serviceworker persistence

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,13 @@
 // client/src-sw.js:
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 // Filter out HMR files
-const manifest = self.__WB_MANIFEST.filter(entry => !entry.url.includes('hot-update'));
-precacheAndRoute(manifest);
+precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -32,16 +31,10 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 // ✅: Implement asset caching
 // Cache assets like JS, CSS, and images
 registerRoute(
-  // 🆕Define the callback function that will filter the requests we want to cache
+  // 🆕 Define the callback function that will filter the requests we want to cache
   // Filter out HMR updates for JS and CSS files
-  ({ request, url }) => {
-    if (url.pathname.endsWith('hot-update.js') || url.pathname.endsWith('hot-update.json')) {
-      return false;
-    }
-    return ['style', 'script', 'worker', 'image'].includes(request.destination);
-  },
-  new CacheFirst({
-    cacheName: 'asset-cache',
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
@@ -53,4 +46,3 @@ registerRoute(
     ],
   })
 );
-
