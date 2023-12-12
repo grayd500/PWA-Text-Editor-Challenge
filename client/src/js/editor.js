1261ad8344572d0ca2ad23decc3ff0176a5c5f6d
David <grayd500@gmail.com>
2023-12-12 15:46:09 -0500

    Fix editor in client director for browser functionality

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,21 +1,48 @@
 // client/src/js/editor.js:
-// Import methods to save and get data from the indexedDB database in './database.js'
-import { EditorView, basicSetup } from '@codemirror/basic-setup';
-import { javascript } from '@codemirror/lang-javascript';
+// Importing methods to interact with the indexedDB database from './database.js'
+import { getDb, putDb } from './database';
+// Importing the 'header' variable from './header.js'
+import { header } from './header';
 
-export default class Editor {
+// Creating a class for handling the CodeMirror editor and data interactions
+export default class {
   constructor() {
-    // Initialize the editor with CodeMirror 6
-    this.editor = new EditorView({
-      doc: 'Type here...',  // Placeholder text
-      extensions: [
-        basicSetup,  // Basic setup with default keymap, input rules, etc.
-        javascript(),  // JavaScript language support
-      ],
-      parent: document.querySelector('#main')  // The DOM element to mount the editor on
+    // Retrieving data from localStorage
+    const localData = localStorage.getItem('content');
+
+    // Checking if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      // Throwing an error if CodeMirror is not loaded
+      throw new Error('CodeMirror is not loaded');
+    }
+
+    // Initializing CodeMirror editor with specified options
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
+
+    // When the editor is ready, set its value from indexedDB, localStorage, or use the default 'header' value
+    getDb().then((data) => {
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    });
+
+    // Saving the content of the editor to localStorage on every change
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
+
+    // Saving the content of the editor to indexedDB when the editor loses focus
+    this.editor.on('blur', () => {
+      console.log('The editor has lost focus');
+      putDb(localStorage.getItem('content'));
     });
   }
 }
-
-// Instantiate the Editor class when the script loads
-new Editor();
