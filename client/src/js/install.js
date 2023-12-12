609301f05fc0f15ef78fce5617ec5241ba49b6f6
David <grayd500@gmail.com>
2023-12-12 15:52:26 -0500

    Update install.js for client to fix button issue

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,29 +8,26 @@ window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = event;
-  // Update UI to notify the user they can add to home screen
-  butInstall.style.display = 'block';
+  // remove the hidden class from install button container
+  butInstall.classList.toggle('hidden', false);
 });
 
 // ✅: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', () => {
-  // Hide our user interface that shows our A2HS button
-  butInstall.style.display = 'none';
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-  });
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+    return;
+  }
+  // show the install prompt
+  promptEvent.prompt();
+  window.deferredPrompt = null;
+  // hide the install button
+  butInstall.classList.toggle('hidden', true);
 });
 
 // ✅: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   // Log install to analytics
+  window.deferredPrompt = null;
   console.log('PWA was installed');
 });
